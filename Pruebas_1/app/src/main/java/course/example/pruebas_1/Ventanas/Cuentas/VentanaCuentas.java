801361c2b9d9ee9a34f313fe2b3e722ea9cd8357
Chrysthoper <chrys_18@hotmail.com>
2016-05-04 20:29:02 -0700

    FUCK

---
+++ Pruebas_1/app/src/main/java/course/example/pruebas_1/Ventanas/Cuentas/VentanaCuentas.java
@@ -0,0 +1,16 @@
+package course.example.pruebas_1.Ventanas.Cuentas;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import course.example.pruebas_1.R;
+
+public class VentanaCuentas extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_cuentas);
+    }
+
+}
---
+++ Pruebas_1/app/src/main/java/course/example/pruebas_1/Ventanas/Cuentas/abcCuentas.java
@@ -0,0 +1,16 @@
+package course.example.pruebas_1.Ventanas.Cuentas;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import course.example.pruebas_1.R;
+
+public class abcCuentas extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ventana_cuentas_abc);
+    }
+
+}
